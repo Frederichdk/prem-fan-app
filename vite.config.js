e09bbdce797cc05474cc3effced130dcb45585ee
Frederich de Koker <dekokerfw@gmail.com>
2025-09-29 13:37:52 -0400

    Update favicon and title in index.html; remove API proxy configuration in vite.config.js; add Premier League SVG logo

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,5 @@ export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api-football": {
-        target: "https://api.football-data.org",
-        changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/api-football/, ""),
-      },
-    },
   },
 });
